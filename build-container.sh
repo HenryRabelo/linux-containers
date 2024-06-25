3bc78937fc44c3d5928c167e07e05677e1b8acea
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2024-06-25 18:54:50 -0300

    Update build-container.sh

--- build-container.sh
+++ build-container.sh
@@ -24,7 +24,8 @@ for IMAGE in $@; do
   fi
   
   mkdir -p "$HOME/.docker/$CONTAINER_NAME"
-  docker container rm $CONTAINER_NAME && docker image rm $IMAGE
+  docker container stop $CONTAINER_NAME && docker container remove $CONTAINER_NAME && docker image remove $IMAGE
+  echo '__________________________________________________'
   docker build $BUILD_OPTS --tag $IMAGE "$(pwd)/$IMAGE-dockerfile/"
   docker run --name $CONTAINER_NAME --interactive --tty --detach $RUN_OPTS --volume "$HOME/.docker/$CONTAINER_NAME:/home/shared" $IMAGE
   echo -e '#!/bin/sh\n'"docker start $CONTAINER_NAME && docker attach $CONTAINER_NAME" > "$HOME/.local/bin/run-$IMAGE" && chmod +x "$HOME/.local/bin/run-$IMAGE"
