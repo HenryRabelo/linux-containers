de125fcb37992fce0405a86094f7556c39cce482
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2026-05-15 10:56:20 -0300

    Improve script quoting and other details

--- build-container.sh
+++ build-container.sh
@@ -1,55 +1,63 @@
 #!/bin/bash
-echo 'Available builds: kali ubuntu fedora opensuse'
-echo 'Build container cmd ex.: ./build-container.sh kali'
-echo 'Run container cmd ex.: run-kali'
-echo '__________________________________________________'
 
-mkdir -p "$HOME/.local/bin"
-mkdir -p "$HOME/.docker"
-
-for DISTRO in $@; do
-  
-  if [ $DISTRO == 'kali' ]; then
-    IMAGE='kalilinux/kali-rolling'
-    CONTAINER='Kali'
-    BUILD_OPTS='--no-cache --force-rm'
-    RUN_OPTS="--network host --hostname tester --user $DISTRO"
-  
-  elif [ $DISTRO == 'ubuntu' ]; then
-    IMAGE="$DISTRO"
-    CONTAINER='Ubuntu'
-    BUILD_OPTS='--force-rm'
-    RUN_OPTS="--hostname coder --user $DISTRO"
-  
-  elif [ $DISTRO == 'fedora' ]; then
-    IMAGE="$DISTRO"
-    CONTAINER='Fedora'
-    BUILD_OPTS='--force-rm'
-    RUN_OPTS="--hostname coder --user $DISTRO"
-  
-  elif [ $DISTRO == 'opensuse' ]; then
-    IMAGE='opensuse/tumbleweed'
-    CONTAINER='openSUSE'
-    BUILD_OPTS='--force-rm'
-    RUN_OPTS="--hostname coder --user $DISTRO"
-  
-  else
-    echo 'Invalid input. Verify your spelling or the current available builds.'
-    exit 1
-  fi
-  
-  if [ ! -f "$(pwd)/$DISTRO-dockerfile/Dockerfile" ]; then
-    echo 'Dockerfile not reachable! Make sure to run this script from its root directory.'
-    exit 1
-  fi
-  
-  mkdir -p "$HOME/.docker/$CONTAINER"
-  docker container stop $CONTAINER && docker container remove $CONTAINER && docker image remove $DISTRO-build
+Create() {
+  echo 'Available builds: ubuntu fedora opensuse kali'
+  echo 'Build container cmd ex.: ./build-container.sh ubuntu'
+  echo 'Run container cmd ex.: run-ubuntu'
   echo '__________________________________________________'
   
-  docker pull $IMAGE &&\
-  docker build $BUILD_OPTS --tag $DISTRO-build "$(pwd)/$DISTRO-dockerfile/" &&\
-  docker create --name $CONTAINER --interactive --tty $RUN_OPTS --volume "$HOME/.docker/$CONTAINER:/home/shared" $DISTRO-build &&\
-  echo -e '#!/bin/sh\n'"docker start $CONTAINER && docker attach $CONTAINER" > "$HOME/.local/bin/run-$DISTRO" && chmod +x "$HOME/.local/bin/run-$DISTRO"
+  mkdir -p "$HOME/.local/bin"
+  mkdir -p "$HOME/.docker"
   
-done
+  for DISTRO in "$@"; do
+  
+    local IMAGE CONTAINER BUILD_OPTS RUN_OPTS
+    
+    if [ "$DISTRO" = 'ubuntu' ]; then
+      IMAGE="$DISTRO"
+      CONTAINER='Ubuntu'
+      BUILD_OPTS='--force-rm'
+      RUN_OPTS="--hostname coder --user $DISTRO"
+    
+    elif [ "$DISTRO" = 'fedora' ]; then
+      IMAGE="$DISTRO"
+      CONTAINER='Fedora'
+      BUILD_OPTS='--force-rm'
+      RUN_OPTS="--hostname coder --user $DISTRO"
+    
+    elif [ "$DISTRO" = 'opensuse' ]; then
+      IMAGE='opensuse/tumbleweed'
+      CONTAINER='openSUSE'
+      BUILD_OPTS='--force-rm'
+      RUN_OPTS="--hostname coder --user $DISTRO"
+      
+    elif [ "$DISTRO" = 'kali' ]; then
+      IMAGE='kalilinux/kali-rolling'
+      CONTAINER='Kali'
+      BUILD_OPTS='--no-cache --force-rm'
+      RUN_OPTS="--network host --hostname tester --user $DISTRO"
+    
+    else
+      echo 'Invalid input. Verify your spelling or the current available builds.'
+      exit 1
+    fi
+    
+    if [ ! -f "$(pwd)/$DISTRO-dockerfile/Dockerfile" ]; then
+      echo 'Dockerfile not reachable! Make sure to run this script from its root directory.'
+      exit 1
+    fi
+    
+    mkdir -p "$HOME/.docker/$CONTAINER"
+    
+    docker container stop "$CONTAINER" && docker container remove "$CONTAINER" && docker image remove "$DISTRO-build"
+    echo '__________________________________________________'
+    
+    docker pull "$IMAGE:latest" &&\
+    docker build "$BUILD_OPTS" --tag "$DISTRO-build" "$(pwd)/$DISTRO-dockerfile/" &&\
+    docker create --name "$CONTAINER" --interactive --tty "$RUN_OPTS" --volume "$HOME/.docker/$CONTAINER:/home/shared" "$DISTRO-build" &&\
+    echo -e '#!/bin/sh\n'"docker start $CONTAINER && docker attach $CONTAINER" > "$HOME/.local/bin/run-$DISTRO" && chmod +x "$HOME/.local/bin/run-$DISTRO"
+    
+  done
+}
+
+Create "$@"
